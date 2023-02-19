1de41e2627e81b8d3888d8c8a4c5fa358e58df66
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-02-19 14:03:47 -0300

    test: add test to TodoItem model

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,4 +2,6 @@
 
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+
+  validates :name, presence: true
 end
--- test/models/todo_item_test.rb
+++ test/models/todo_item_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class TodoItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'invalid todo item' do
+    todo_list = todo_lists(:one)
+
+    todo_item = TodoItem.new(todo_list: todo_list)
+
+    assert_not todo_item.valid?
+  end
+
+  test 'valid todo item' do
+    todo_list = todo_lists(:one)
+
+    todo_item = TodoItem.new(todo_list: todo_list, name: 'Name')
+
+    assert_predicate todo_item, :valid?
+  end
 end
