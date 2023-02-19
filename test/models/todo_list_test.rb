a6130d462917b9bc938009f3c27782edd211861d
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-02-19 10:28:49 -0300

    test: add a test to todo list model

--- test/models/todo_list_test.rb
+++ test/models/todo_list_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class TodoListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid todo list' do
+    todo_list = TodoList.new name: 'Todo List #1'
+
+    assert_predicate(todo_list, :valid?)
+  end
+
+  test 'invalid todo list' do
+    todo_list = TodoList.new
+
+    assert_not todo_list.valid?
+  end
 end
